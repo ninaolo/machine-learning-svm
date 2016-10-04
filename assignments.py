94ef59e8347a7501f9848e4f46986875f4bcdfd0
Nina Olofsson <Nina@Ninas-MBP.lan>
2016-10-04 19:47:36 +0200

    Add plot and random data

--- assignments.py
+++ assignments.py
@@ -15,5 +15,29 @@ import numpy, pylab, random, math
 
 
 def linear_kernel(x, y):
+    """ Returns the linear kernel function of two vectors x and y. """
     return numpy.dot(x, y) + 1
 
+
+def create_random_classified_test_data():
+    """ Creates random data points (x, y) with two classes -1 and 1. """
+
+    class_a = [(random.normalvariate(-1.5, 1), random.normalvariate(0.5, 1), 1.0) for i in range(5)] + [
+        (random.normalvariate(1.5, 1), random.normalvariate(0.5, 1), 1.0) for i in range(5)]
+
+    class_b = [(random.normalvariate(0.0, 0.5), random.normalvariate(-0.5, 0.5), -1.0) for i in range(10)]
+
+    data = class_a + class_b
+    random.shuffle(data)
+
+    return class_a, class_b
+
+
+def plot_data(class_a, class_b):
+    """ Plots two classes of data. """
+    pylab.hold(True)
+
+    pylab.plot([p[0] for p in class_a], [p[1] for p in class_a], 'bo')
+    pylab.plot([p[0] for p in class_b], [p[1] for p in class_b], 'ro')
+
+    pylab.show()
